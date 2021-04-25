a305fe22e44c402668b242936204b42821b804db
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-25 13:53:30 +0300

    Added fetch request to get contacts data.

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch("https://randomuser.me/api/?results=100");
+        const { results, error } = await response.json();
+
+        if (error) {
+          throw new Error(error);
+        }
+
+        setData(results);
+        setIsError(false);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getContacts();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+};
 
 export default function Contacts() {
+  const contacts = useContacts();
+
+  if (contacts.isLoading) {
+    return (
+      <div>
+        <p>...Loading...</p>
+      </div>
+    );
+  }
+
+  if (contacts.isError) {
+    return (
+      <div>
+        <p>...Error...</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h1>Contacts</h1>
+      <h1>Contacts {contacts.data[0].name.first}</h1>
     </div>
   );
 }
